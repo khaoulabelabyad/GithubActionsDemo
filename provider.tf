ccb61d709a4778e018ab02868b3f5fd872720340
Khaoula Belabyad <belabyad.khaoula@gmail.com>
2024-10-03 09:52:26 +0000

    Edit deploy.yml and provider.tf to include azure_credentials

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = jsondecode(var.azure_credentials).subscriptionId
+  client_id       = jsondecode(var.azure_credentials).clientId
+  client_secret   = jsondecode(var.azure_credentials).clientSecret
+  tenant_id       = jsondecode(var.azure_credentials).tenantId
 }
+
+variable "azure_credentials" {}
+
