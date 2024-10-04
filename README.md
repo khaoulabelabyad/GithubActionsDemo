1ed883248d735913f2a334066e06248019c6418d
Khaoula Belabyad <belabyad.khaoula@gmail.com>
2024-10-04 14:03:22 +0000

    Creation of state.tf - state storage account creation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Terraform init
+
+terraform init -var
---
+++ state.tf
@@ -0,0 +1,22 @@
+resource "azurerm_storage_account" "state" {
+  name                      = "gitactionsstatekbtest"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  account_tier    = "Standard"
+  account_replication_type  = "LRS"
+}
+
+resource "azurerm_storage_container" "state_container" {
+  name                  = "github-actions-state"
+  storage_account_name  = azurerm_storage_account.state.name
+  container_access_type = "private"
+}
+
+terraform{
+    backend "azurerm" {
+      resource_group_name  = azurerm_resource_group.rg.name
+      storage_account_name = azurerm_storage_account.state.name
+      container_name       = azurerm_storage_container.state_container.name
+      key                  = "vm_provisioning/terraform.tfstate"
+  }
+}
