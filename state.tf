f7ef665647895917854f0d342637972f6caa53a4
Khaoula Belabyad <belabyad.khaoula@gmail.com>
2024-10-07 09:04:09 +0000

    Add backend.tf file

--- state.tf
+++ state.tf
@@ -3,12 +3,3 @@ resource "azurerm_storage_container" "state_container" {
   storage_account_name  = data.azurerm_storage_account.state.name
   container_access_type = "private"
 }
-
-terraform{
-    backend "azurerm" {
-      resource_group_name  = "github-actions-resources"
-      storage_account_name = "gitactionsstatekbtest"
-      container_name       = "github-actions-state"
-      key                  = "vm_provisioning/terraform.tfstate"
-  }
-}
