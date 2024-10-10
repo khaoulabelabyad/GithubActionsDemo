ea9943e00de120863114fd1e1e34d55559b58175
Khaoula Belabyad <belabyad.khaoula@gmail.com>
2024-10-10 10:42:16 +0000

    Fix Terraform Plan - loop

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform{
-#     backend "azurerm" {
-#       resource_group_name  = "github-actions-resources"
-#       storage_account_name = "gitactionsstatekbtest"
-#       container_name       = "github-actions-state"
-#       key                  = "vm_provisioning/terraform.tfstate"
-#   }
-# }
--- data.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_resource_group" "rg" {
-  name     = "github-actions-resources"
-  location = "Italy North"
-}
-
-data "azurerm_storage_account" "state" {
-  name = "gitactionsstatekbtest"
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
