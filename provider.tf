d89b45d57cac32aa2d79ff72622a3bb965b593eb
Khaoula Belabyad <belabyad.khaoula@gmail.com>
2024-10-10 16:02:05 +0000

    Edit provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+
+    }
+
+  }
+
+  backend "azurerm" {
+      resource_group_name  = "github-actions-resources"
+      storage_account_name = "gitactionsstatekbtest"
+      container_name       = "github-actions-state"
+      key                  = "vm_provisioning/terraform.tfstate"
+  }
+} 
+
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
+
 
 
